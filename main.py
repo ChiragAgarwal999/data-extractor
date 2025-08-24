4e649f5cb92cc610408181553cbef51a4661e707
Chirag Agarwal <agarwalchirag999@gmail.com>
2025-08-24 22:38:12 +0530

    port update

--- main.py
+++ main.py
@@ -90,4 +90,7 @@ def generate_excel(shop, location):
 def download_file(filename):
     return send_from_directory(OUTPUT_FOLDER, filename, as_attachment=True)
 
-app.run(debug=True)
+# app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT
+    app.run(host="0.0.0.0", port=port, debug=False)
