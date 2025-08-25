54fa028879370d6fdb5e53d7f3e660e4e97270b2
Chirag Agarwal <agarwalchirag999@gmail.com>
2025-08-25 11:33:41 +0530

    update port

--- main.py
+++ main.py
@@ -92,5 +92,5 @@ def download_file(filename):
 
 # app.run(debug=True)
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render sets PORT
+    port = int(os.environ.get("PORT", 8080))  # Render sets PORT
     app.run(host="0.0.0.0", port=port, debug=False)
